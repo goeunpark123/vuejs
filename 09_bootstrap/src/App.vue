060b793474040a23bcba758366538046a9256e31
goeunpark123 <64297541+goeunpark123@users.noreply.github.com>
2024-06-13 16:44:06 +0900

    Add: 09_bootstrap

---
+++ 09_bootstrap/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <b-badge pill variant="primary">Primary</b-badge>
+    <b-badge pill variant="secondary">Secondary</b-badge>
+    <b-badge pill variant="success">Success</b-badge>
+    <b-badge pill variant="danger">Danger</b-badge>
+    <b-badge pill variant="warning">Warning</b-badge>
+    <b-badge pill variant="info">Info</b-badge>
+    <b-badge pill variant="light">Light</b-badge>
+    <b-badge pill variant="dark">Dark</b-badge>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
