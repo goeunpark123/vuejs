4d0f0ce7fe3803fd90dc6710c6792439f4f1e5a4
goeunpark123 <64297541+goeunpark123@users.noreply.github.com>
2024-06-13 16:42:54 +0900

    Add: 06_slot

---
+++ 06_slot/src/components/04_dynamicslot/DynamicParent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="parentContainer">
+        <h1>DynamicSlot: </h1>
+        <input type="text" v-model="dynamicName">
+        <DynamicChild>
+            {{ dynamicName }}
+        </DynamicChild>
+    </div>
+
+</template>
+
+<script setup>
+    import { ref } from 'vue';
+</script>
+
+<style scoped>
+    .parentContainer {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        border: 1px solid black;
+    }
+</style>
